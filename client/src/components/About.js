661ebabdb0d1104fa6298376c57a3e7f65564791
Jayce Turambe <jnn56@txstate.edu>
2024-04-25 01:34:39 -0500

    Updated the send message

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -14,7 +14,7 @@ function About() {
                     Almost everything we use and interact with today utilizes some form of computer science. 
                     Think about it: What device are you on while reading this story? What framework runs 
                     the website this story is on? How is your device connected to the internet that connects 
-                    you to us?! We're just getting started. Due to computer science, society has undergone 
+                    you to us?! We're just getting started.<br/><br/> Due to computer science, society has undergone 
                     a number of changes. Artificial intelligence, for instance. AI helps doctors detect 
                     diseases. Using self-driving cars reduces the use of fossil fuels and takes your hands off 
                     the wheel. Programs that project the spread of forest fires and the most efficient way to 
@@ -24,7 +24,7 @@ function About() {
                     and they will continue to become more pronounced.<br/><br/>
                     How can you ensure that you have a fair shot at success in this new expanding economy?
                     In today's digital era, the demand for skilled tech professionals is at an all-time high. 
-                    ode has become the language of the 21st century, driving innovation and shaping the future of industries worldwide.<br/><br/> 
+                    Code has become the language of the 21st century, driving innovation and shaping the future of industries worldwide.<br/><br/> 
                     Recognizing the transformative power of technology, I chose to pursue a career in the dynamic field of computer programming. 
                     Whether through formal education or alternative learning paths, I've dedicated myself to mastering the latest technologies 
                     and staying ahead of the curve. <br/><br/> By continuously honing my skills and embracing the ever-evolving tech landscape, 
